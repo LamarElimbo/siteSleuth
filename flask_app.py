6d62c5dfcc291f6ee22335a71ab6ba6d5d950383
lamar133 <lamar_johnson133@yahoo.ca>
2017-10-11 15:20:13 -0400

    First commit

---
+++ flask_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from string import Template
+import main
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/search.html')
+def search():
+    return render_template('search.html')
+
+@app.route('/result.html', methods=['POST'])
+def result():
+    website = request.form['website']
+    requiredInfo = main.runSearch(website)
+    return render_template('result.html', site=website, title=requiredInfo['Title'], description=requiredInfo['Description'], socials=requiredInfo['Socials'], name =requiredInfo["Admin's Name"], address=requiredInfo['Address'], city=requiredInfo['City'], state=requiredInfo['State'], country=requiredInfo['Country'], phone=requiredInfo['Phone'], email=requiredInfo['Email'], alexa_score=requiredInfo['Alexa Score'], keywords=requiredInfo['Keywords'], timezone_id=requiredInfo['Timezone ID'], timezone_name=requiredInfo['Timezone Name'])
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
---
+++ main.py
@@ -0,0 +1,26 @@
+# Main function to call and collect all useful information in dictionary
+
+import sliceURL
+import scrapeMain
+import scrapeWhoIs
+import scrapeAlexa
+import getTimezone
+
+def runSearch(pURL):
+    
+    slicedURL = sliceURL.sliceURL(pURL)
+    siteTitle, siteDescription, socialsOnSite = scrapeMain.scrapeMain(pURL)
+    adminName, adminAddress, adminCity, adminStateOrProvince, adminCountry, adminPhone, adminEmail = scrapeWhoIs.scrapeWhoIs(pURL)
+    score, keywords = scrapeAlexa.scrapeAlexa(slicedURL)
+    
+    timezoneId=''
+    timezoneName=''
+    if adminCity == 'No city found.' or adminStateOrProvince == 'No state or province found.':
+        timezoneId = 'No timezone ID found.'
+        timezoneName = 'No timezone name found.'
+    else:
+        timezoneId, timezoneName = getTimezone.getTimezone(adminCity, adminStateOrProvince)
+        
+    requiredInfo = {'Title': siteTitle, 'Description': siteDescription, 'Socials': socialsOnSite, "Admin's Name": adminName, 'Address': adminAddress, 'City': adminCity, 'State': adminStateOrProvince, 'Country': adminCountry, 'Phone': adminPhone, 'Email': adminEmail, 'Alexa Score': score, 'Keywords': keywords, 'Timezone ID': timezoneId, 'Timezone Name': timezoneName}
+    
+    return requiredInfo
